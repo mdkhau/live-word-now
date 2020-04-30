07403fd343f08680da5ea6b6340ee3a75980e94e
mdkhau <git>
2020-04-30 20:26:33 +0300

    Updates

---
+++ precache-manifest.9f67a6ffdc9eadd93bd13351f0e00525.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bee8185c06265ef01b33e7f8af16b02",
+    "url": "/live-word-now/index.html"
+  },
+  {
+    "revision": "d33f1e59976c8e1c75da",
+    "url": "/live-word-now/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "95b17192381531a03daf",
+    "url": "/live-word-now/static/js/2.4f488296.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/live-word-now/static/js/2.4f488296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d33f1e59976c8e1c75da",
+    "url": "/live-word-now/static/js/main.4fe3a74b.chunk.js"
+  },
+  {
+    "revision": "1f0b949682b56ed438eb",
+    "url": "/live-word-now/static/js/runtime-main.33045d70.js"
+  }
+]);
